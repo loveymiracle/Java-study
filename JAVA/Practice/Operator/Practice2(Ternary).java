620f1c52d325e9312bf37bc6619a3dd0b9b288f9
loveymiracle <86279407+loveymiracle@users.noreply.github.com>
2023-11-07 12:55:46 +0900

    Create Practice2(Ternary).java

---
+++ JAVA/Practice/Operator/Practice2(Ternary).java
@@ -0,0 +1,14 @@
+	public void practice2() {
+		// 키보드로 입력 받은 하나의 정수가 양수이면 "양수다", 양수가 아니면 "양수가 아니다"를 출력하세요.
+		// ※ 삼항 연산자 사용해주세요!
+		
+		// 정수 : -9
+		// 양수가 아니다 
+		System.out.println("practice2!!");
+		
+		System.out.println("정수 : ");
+		int num = Integer.parseInt(sc.nextLine());
+		
+		String result = num > 0 ? "양수다" : "양수가 아니다";
+		System.out.println(result);
+	}
