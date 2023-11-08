c56c0e10c36e7110800cbb1bc404920702a9c1df
loveymiracle <86279407+loveymiracle@users.noreply.github.com>
2023-11-08 16:48:46 +0900

    Create LoopStatement4.java

---
+++ JAVA/basic/ControlStatement/Loop/LoopStatement4.java
@@ -0,0 +1,22 @@
+
+public class LoopStatement4 {
+	public static void main(String[] args) {
+		// for 문 응용 -> 2씩 증가하는 반복문
+		for(int i = 2; i <= 20; i += 2) {
+			System.out.println(i);
+		}
+		
+		// for문 조건 지정 안하기 가능!
+		for(int i = 0; ; i += 2) {
+			System.out.println(i);
+			if(i == 100) {
+				break;
+			}
+		}
+		
+		// for문 무한 반복문 만들기  (이것 보단 while로 만드는게 일반적)
+//		for(;;) {
+//			System.out.println("무한루프");
+//		}
+	}
+}
