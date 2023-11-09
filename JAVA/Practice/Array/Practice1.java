9e3a8e391cd116b4ed761513cd5c7aa7b8aed23e
loveymiracle <86279407+loveymiracle@users.noreply.github.com>
2023-11-09 13:58:00 +0900

    Update Practice1.java

--- JAVA/Practice/Array/Practice1.java
+++ JAVA/Practice/Array/Practice1.java
@@ -22,7 +22,7 @@ public class ArrayPractice {
 		  ap.practice10();
 	}
 	
-	public void practice1() {
+	public static void practice1() {
 		// 길이가 10인 배열을 선언하고 1부터 10까지의 값을 반복문을 이용하여 순서대로 배열 인덱스에 넣은 후 그 값을 출력하세요.
     // 1 2 3 4 5 6 7 8 9 10
 		int array[] = new int[10];
