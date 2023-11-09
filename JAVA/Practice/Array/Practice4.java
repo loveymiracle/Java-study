f21034384e650e5f00a7b972145fcf11730cabe2
loveymiracle <86279407+loveymiracle@users.noreply.github.com>
2023-11-09 13:59:53 +0900

    Create Practice4.java

---
+++ JAVA/Practice/Array/Practice4.java
@@ -0,0 +1,5 @@
+public static void practice4() {
+		// 길이가 5인 String배열을 선언하고 "사과", "귤", "포도", "복숭아", "참외"로 초기화 한 후 배열 인덱스를 활용해서 귤을 출력하세요.
+		String strArray[] = { "사과", "귤", "포도", "복숭아", "참외" };
+		System.out.println(strArray[1]);
+	}
