a790acaa880b2edfdde150ed1966fdd9bbcdf690
loveymiracle <86279407+loveymiracle@users.noreply.github.com>
2023-11-13 17:34:17 +0900

    Create Run.java

---
+++ JAVA/Practice/ObjectArray/Run.java
@@ -0,0 +1,12 @@
+package hw.member.run;
+
+import hw.member.view.MemberMenu;
+
+public class Run {
+	
+	public static void main(String[] args) {
+		MemberMenu mm = new MemberMenu();
+		
+		mm.mainMenu();
+	}
+}
