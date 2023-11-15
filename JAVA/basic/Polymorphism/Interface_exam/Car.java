61fb2a98155d08e521ef6cb73a3bbe460ff1b5f2
loveymiracle <86279407+loveymiracle@users.noreply.github.com>
2023-11-15 20:15:36 +0900

    Create Car.java

---
+++ JAVA/basic/Polymorphism/Interface_exam/Car.java
@@ -0,0 +1,30 @@
+package hw.ex04.interface_exam;
+
+// 추상 클래스화
+// -> 부모는 추상클래스로 생성하지 못하게 만드는 것이 좋다.
+public abstract class Car {
+	protected String name;
+	protected int cc = 0;
+	
+	public Car(String name, int cc) {
+		this.name = name;
+		this.cc = cc;
+	}
+	
+	public void oilling() {
+		System.out.println("주유중 입니다.");
+	}
+	
+	public void move() {
+		System.out.println("부릉부릉");
+	}
+	
+	@Override
+	public String toString() {
+		String str = "";
+		str += "name : " + name + ", ";
+		str += "Classname : " + this.getClass().getSimpleName() + ", ";
+		str += "cc : " + cc;
+		return str;
+	}
+}
