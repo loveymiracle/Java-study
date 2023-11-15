5f699e7a5261257f11f68d5a8e77c43b300bb1ff
loveymiracle <86279407+loveymiracle@users.noreply.github.com>
2023-11-15 20:17:08 +0900

    Create Grandeur.java

---
+++ JAVA/basic/Polymorphism/Interface_exam/Grandeur.java
@@ -0,0 +1,28 @@
+package hw.ex04.interface_exam;
+
+public class Grandeur extends HyundaiCar{
+
+	public Grandeur() {
+		super("그랜저", 3000); // 3000cc
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void oilling() {
+		System.out.println("휘발유를 주유합니다.");
+	}
+	
+	@Override
+	public void move() {
+		System.out.println("(조용히)위이이이잉");
+	}
+	
+//	public void grandeurNickName() {
+//		System.out.println("각그랜저");
+//	}
+	
+	@Override
+	public void nickName() {
+		System.out.println("각그랜저");
+	}
+}
