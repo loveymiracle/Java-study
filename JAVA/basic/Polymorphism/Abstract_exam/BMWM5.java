8cc4d089ce543d8208c157ba9945cfd45b791c03
loveymiracle <86279407+loveymiracle@users.noreply.github.com>
2023-11-15 19:02:29 +0900

    Create BMWM5.java

---
+++ JAVA/basic/Polymorphism/Abstract_exam/BMWM5.java
@@ -0,0 +1,22 @@
+package hw.ex03.abstract_exam;
+
+public class BMWM5 extends Car{
+
+	public BMWM5() {
+		super("BMW M5", 4395); // 4395cc
+	}
+	
+	@Override
+	public void oilling() {
+		System.out.println("고급 휘발유를 주유중입니다.");
+	}
+	
+	@Override
+	public void move() {
+		System.out.println("부아아아아앙(시끄럽다.)");
+	}
+	
+	public void nickName() {
+		System.out.println("독삼사의 좋은차");
+	}
+}
