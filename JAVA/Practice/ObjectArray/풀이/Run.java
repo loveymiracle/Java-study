333d6719ca3a8933f396a61ca8dae92cc50badda
loveymiracle <86279407+loveymiracle@users.noreply.github.com>
2023-11-20 17:55:38 +0900

    Create Run.java

---
+++ JAVA/Practice/ObjectArray/풀이/Run.java
@@ -0,0 +1,11 @@
+package hw.member.run;
+
+import hw.member.view.MemberMenu;
+
+public class Run {
+	
+	public static void main(String[] args) {
+	MemberMenu mm =  new MemberMenu();
+	mm.mainMenu();
+	}
+}
