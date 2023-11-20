131a45a80d0ba7ff2ec8f88f7573ad7593e49e72
loveymiracle <86279407+loveymiracle@users.noreply.github.com>
2023-11-20 18:09:28 +0900

    Create Run.java

---
+++ JAVA/Practice/Poly/풀이/Run.java
@@ -0,0 +1,12 @@
+package hw.poly.run;
+
+import com.multi.poly.view.LibraryMenu;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		LibraryMenu lm = new LibraryMenu();
+		lm.mainMenu();
+	}
+}
