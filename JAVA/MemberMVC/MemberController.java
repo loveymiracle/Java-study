4a77928bafd3be4361823e1c8fcba2e08c36d9da
loveymiracle <86279407+loveymiracle@users.noreply.github.com>
2023-12-21 16:56:06 +0900

    Create MemberController.java

---
+++ JAVA/MemberMVC/MemberController.java
@@ -0,0 +1,38 @@
+package member.controller;
+
+import java.util.List;
+
+import member.model.service.MemberService;
+import member.model.vo.Member;
+
+// Controller : Spring에서는 url을 통해 요청을 받고 서비스를 다중으로 보유할 수 있는 객체
+public class MemberController {
+	
+	MemberService service = new MemberService();
+//	BoardService boardService = new BoardService();
+//	ShopService shopService = new ShopService();
+	
+	public List<Member> getAllMemberList() {
+		return service.getAllMemberList();
+	}
+
+	public Member searchById(String memberId) {
+		return service.searchById(memberId);
+	}
+
+	public List<Member> searchByName(String memberName) {
+		return service.searchByName(memberName);
+	}
+
+	public int insertMember(Member member) {
+		return service.insertMember(member);
+	}
+
+	public int updateMember(Member member) {
+		return service.updateMember(member);
+	}
+
+	public int deleteMember(String memberId) {
+		return service.deleteMember(memberId);
+	}
+}
