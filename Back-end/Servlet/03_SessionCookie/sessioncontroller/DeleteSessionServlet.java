cba601a19a4d63f5ae02416012f2c26122597859
Yabu <86279407+loveymiracle@users.noreply.github.com>
2024-01-08 13:55:47 +0900

    Create DeleteSessionServlet.java

---
+++ Back-end/Servlet/03_SessionCookie/sessioncontroller/DeleteSessionServlet.java
@@ -0,0 +1,24 @@
+package com.session.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/deletesession.do")
+public class DeleteSessionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 세션 삭제하는 방법
+		req.getSession().invalidate();
+		
+		resp.setContentType("text/html;charset=utf-8");
+		resp.getWriter().append("<script> alert('세션이 삭제되었습니다.'); "
+				+ "location.href='" + req.getContextPath()+ "' </script>");
+	}
+}
