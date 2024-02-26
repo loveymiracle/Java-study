19d213cffcfabd070e5d94475c8f7a283695b0d3
Yabu <86279407+loveymiracle@users.noreply.github.com>
2024-02-26 14:36:11 +0900

    Create MemberDao.java

---
+++ Back-end/Spring/02_SpringDIMybatis/MemberDao.java
@@ -0,0 +1,15 @@
+package com.mybatis.member.model.dao;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+
+import com.mybatis.member.model.vo.Member;
+
+public interface MemberDao {
+	int insertMember(SqlSessionTemplate session, Member member);
+	List<Member> selectMemberAll(SqlSessionTemplate session);
+	Member selectMemberById(SqlSessionTemplate session, String id);
+	int deleteMember(SqlSessionTemplate session, String id);
+	
+}
