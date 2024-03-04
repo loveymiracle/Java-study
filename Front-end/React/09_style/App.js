8d8408181968201c2dbe0e1e38ad98234dcfe517
Yabu <86279407+loveymiracle@users.noreply.github.com>
2024-03-04 17:31:33 +0900

    Create App.js

---
+++ Front-end/React/09_style/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import SassComponent from './components/SassComponent';
+import CSSModule from './components/CSSModule';
+
+function App() {
+  return (
+    <div>
+      <CSSModule/>
+      <SassComponent />
+    </div>
+  );
+}
+
+export default App;
