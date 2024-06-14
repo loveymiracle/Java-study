c5e7bf0467a27f09981eed753c4ec49299538836
Yabu <86279407+loveymiracle@users.noreply.github.com>
2024-06-14 13:52:13 +0900

    Create Solution2.java

---
+++ JAVA/CodingTest/Basic2/Day01/Solution2.java
@@ -0,0 +1,16 @@
+// Day 1 출력 > a와 b 출력하기
+package Day01;
+
+import java.util.*;
+
+public class Solution2 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+
+}
