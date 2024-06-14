d5c4c02b223b1e4eaf96003e900c668a86e6ea4b
Yabu <86279407+loveymiracle@users.noreply.github.com>
2024-06-14 13:55:08 +0900

    Create Solution5.java

---
+++ JAVA/CodingTest/Basic2/Day01/Solution5.java
@@ -0,0 +1,12 @@
+// Day 1 출력 > 특수문자 출력하기
+package Day01;
+
+import java.util.*;
+
+public class Solution5 {
+	public static void main(String[] args) {
+		String str = "!@#$%^&*(\\'\"<>?:;";
+        System.out.println(str);
+	}
+
+}
