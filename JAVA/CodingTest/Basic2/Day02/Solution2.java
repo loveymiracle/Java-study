b6430be7bb1eaa45d7f4f6e43b70c4fce87883b1
Yabu <86279407+loveymiracle@users.noreply.github.com>
2024-07-11 11:32:30 +0900

    Create Solution2.java

---
+++ JAVA/CodingTest/Basic2/Day02/Solution2.java
@@ -0,0 +1,14 @@
+// Day 2 출력, 연산 > 문자열 붙여서 출력하기
+package Day02;
+
+import java.util.*;
+
+public class Solution2 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        System.out.println(a + b);
+    }
+
+}
