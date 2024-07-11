108fdb0bd9ea6797a7ebc71ca500bc7f2f7a0fb0
Yabu <86279407+loveymiracle@users.noreply.github.com>
2024-07-11 11:32:16 +0900

    Create Solution1.java

---
+++ JAVA/CodingTest/Basic2/Day02/Solution1.java
@@ -0,0 +1,16 @@
+// Day 2 출력, 연산 > 덧셈식 출력하기
+package Day02;
+
+import java.util.*;
+
+public class Solution1 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a+b;
+
+        System.out.println(a + " + " + b + " = " + sum);
+    }
+
+}
