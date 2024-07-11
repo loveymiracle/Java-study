a07133344e32d49b454bc40d46ff97c229eace0b
Yabu <86279407+loveymiracle@users.noreply.github.com>
2024-07-11 11:32:40 +0900

    Create Solution3.java

---
+++ JAVA/CodingTest/Basic2/Day02/Solution3.java
@@ -0,0 +1,15 @@
+// Day 2 출력, 연산 > 문자열 돌리기
+package Day02;
+
+import java.util.*;
+
+public class Solution3 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        for(char c : a.toCharArray()) {
+        	System.out.println(c);
+        }
+    }
+
+}
